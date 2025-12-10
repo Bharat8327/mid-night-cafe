443eac1c9db672f56837db8b3629739b8b8f3a67
Bharat8327 <bp609213@gmail.com>
2025-12-10 11:02:45 +0530

    cross origin url updated

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: process.env.ALLOW_ORIGIN,
   }),
 );
 app.use(morgan('dev')); // Use morgan for logging HTTP requests
